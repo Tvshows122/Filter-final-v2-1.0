18073da30b484b3df92b3bebb9face59b4da2be4
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-05-07 20:49:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tvshows122/Filter-final-v2/tree/main /TheMovieProviderBot
+  git clone https://github.com/Tvshows122/Filter-final-v2/tree/main /mybot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /mybot
 fi
 cd /TheMovieProviderBot
 pip3 install -U -r requirements.txt

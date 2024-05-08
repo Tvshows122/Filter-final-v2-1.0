220a08aebae29d4131ebb7eb4cb1710763409c6e
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-05-08 18:43:29 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2100,7 +2100,7 @@ async def auto_filter(client, msg, spoll=False):
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
         m=await message.reply_sticker("CAACAgQAAxkBAAEKSxplArIUActk4ORQuFn3DHFvBqQCOgACBQMAAnJxFyVYcSIunXgGjjAE",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f'Seaching for {search} 🔎', url=f"https://t.me/MovizTube_Group")]]) 
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f'Seaching for {search} 🔎', url=f"https://t.me/+cRRlHEGxJcszNWY1")]]) 
         )
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
